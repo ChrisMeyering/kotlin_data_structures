65118060ca7cde244264808a2aed216a81ff5a0f
Chris Meyering <christophe.daven@gmail.com>
2020-01-22 16:53:30 -0800

    implemented Stack data structure

--- src/stack/Stack.kt
+++ src/stack/Stack.kt
@@ -1,4 +1,43 @@
 package stack
 
-class Stack {
+class Stack(private val maxSize: Int) {
+    private val s = Array<Any>(maxSize) { 0 }
+    private var top = 0
+
+    init {
+        assert(maxSize > 0)
+    }
+
+    fun isEmpty(): Boolean = top == 0
+
+    fun isFull(): Boolean = top == maxSize
+
+    fun push(element: Any): Boolean {
+        if (isFull()) {
+            return false
+        }
+        s[top++] = element
+        return true
+    }
+
+    fun pop(): Any? {
+        if (isEmpty()) {
+            return null
+        }
+        return s[--top]
+    }
+
+    fun peek(): Any? {
+        if (isEmpty()) {
+            return null
+        }
+        return s[top - 1]
+    }
+
+    fun check() {
+        assert(top > 0)
+        assert(top <= maxSize)
+    }
+
+
 }
