d61e54f6b84f25bf66ea05f301e8219fcb5ad3ee
Chris Meyering <christophe.daven@gmail.com>
2020-01-22 16:27:23 -0800

    added queue.peek to random test

--- src/queue/test.kt
+++ src/queue/test.kt
@@ -9,19 +9,34 @@ fun random_test() {
     var rm = 0
     var addFull = 0
     var rmEmpty = 0
+    var peek = 0
+    var peekEmpty = 0
 
     val q = Queue(N)
     val list = mutableListOf<Int>()
     q.check()
 
-    repeat(21) {
+    repeat(31) {
         val bias =
-            if (it%2 == 0) {
+            if (it % 2 == 0) {
                 0.1
             } else {
                 -0.1
             }
-        repeat(1000000) {
+        repeat(1_000_000) {
+            if (Random.nextDouble() < 0.2) {
+                val element = q.peek()
+                q.check()
+                if (element == null) {
+                    assert(list.size == 0)
+                    assert(q.isEmpty())
+                    peekEmpty++
+                } else {
+                    val expectedElement = list[0]
+                    assert(expectedElement == element)
+                    peek++
+                }
+            }
             if (Random.nextDouble() < 0.5 + bias) {
                 val element = Random.nextInt(2.0.pow(32).toInt())
                 val result = q.enqueue(element)
@@ -49,17 +64,21 @@ fun random_test() {
             }
         }
     }
-    while(!q.isEmpty()) {
+    while (!q.isEmpty()) {
         val element = q.dequeue()
         q.check()
         val expectedElement = list.removeAt(0)
         assert(expectedElement == element)
         rm++
     }
+    assert(q.size() == 0)
+    assert(list.isEmpty())
     println("adds: $add")
     println("adds to full queue: $addFull")
     println("removes: $rm")
     println("removes to empty queue: $rmEmpty")
+    println("peeks: $peek")
+    println("peeks to empty queue: $peekEmpty")
 }
 
 fun main(args: Array<String>) {
